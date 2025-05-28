4391ca9479dae5e73b8c2ad3961517e419a6509b
WangZe <wangze@zb-software.com>
2025-05-28 19:00:14 +0800

    feat：vite init

---
+++ playgrounds/vite/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import {useFetch} from "@wthe/utils-core"
+
+const {execute,abort} =  useFetch('https://jsonplaceholder.typicode.com/posts/1',{ immediate: false });
+</script>
+
+<template>
+  <button @click="execute">
+    发送
+  </button>
+  <button @click="abort">
+    取消
+  </button>
+</template>
+
---
+++ playgrounds/vite/vite.config.ts
@@ -0,0 +1,15 @@
+import { resolve } from 'node:path'
+
+import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@wthe/utils-core': resolve(__dirname, '../../packages/core/index.ts'),
+      '@wthe/utils-shared': resolve(__dirname, '../../packages/shared/index.ts'),
+    },
+  },
+})
