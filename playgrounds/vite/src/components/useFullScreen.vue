6682be727ea21512386f936de4f87d991d8536c0
WangZe <wangze@zb-software.com>
2025-06-04 18:59:13 +0800

    feat：test useFullScreen

---
+++ playgrounds/vite/src/components/useFullScreen.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useFullScreen } from '@wthe/utils-core'
+import { onMounted, ref } from 'vue'
+
+const boxRef = ref()
+
+let toggle: () => void
+
+onMounted(() => {
+  const result = useFullScreen(boxRef.value)
+
+  toggle = result.toggle
+})
+</script>
+
+<template>
+  <button @click="toggle">
+    Toggle Fullscreen
+  </button>
+  <div ref="boxRef" class="box" />
+</template>
+
+<style scoped>
+.box {
+  width: 100px;
+  height: 100px;
+  background-color: red;
+}
+</style>
