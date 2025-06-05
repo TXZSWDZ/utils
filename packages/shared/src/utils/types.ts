a6f3e44a796ab7256ed774aac530a745dfab3a1c
WangZe <wangze@zb-software.com>
2025-06-05 18:35:54 +0800

    types：类型拓展

--- packages/shared/src/utils/types.ts
+++ packages/shared/src/utils/types.ts
@@ -1,5 +1,5 @@
-export type Fn = () => void
-
-export type AsyncFn<T = void> = () => Promise<T>
-
 export type Awaitable<T> = T | Promise<T>
+
+export type Fn<P extends any[] = never, T = void> = (...args: P) => T
+
+export type AsyncFn<P extends any[] = never, T = void> = (...args: P) => Awaitable<T>
