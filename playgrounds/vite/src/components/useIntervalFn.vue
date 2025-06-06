fd3e18fd11f72c4d13349194f98900769df5a69e
WangZe <wangze@zb-software.com>
2025-06-06 11:08:46 +0800

    test：测试useIntervalFn

---
+++ playgrounds/vite/src/components/useIntervalFn.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useIntervalFn } from '@wthe/utils-shared'
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const { start, pause, resume, cancel, getState, getRemaining } = useIntervalFn(() => {
+  count.value++
+}, 6000, false)
+</script>
+
+<template>
+  <button @click="start">
+    开始
+  </button>
+  <button @click="pause">
+    暂停
+  </button>
+  <button @click="resume">
+    继续
+  </button>
+  <button @click="cancel">
+    取消
+  </button>
+  <button @click="() => { console.log(getState()) }">
+    获取状态
+  </button>
+  <button @click="() => { console.log(getRemaining()) }">
+    获取剩余时间
+  </button>
+  <span>
+    Interval function executed：{{ count }}
+  </span>
+</template>
