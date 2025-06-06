aa612c9a8d99fe846ae1ff4d02dae11f00c0dfe1
WangZe <wangze@zb-software.com>
2025-06-06 13:34:10 +0800

    test：测试useWebSocket

--- playgrounds/vite/src/components/useWebSocket.vue
+++ playgrounds/vite/src/components/useWebSocket.vue
@@ -1,34 +1,41 @@
 <script setup lang="ts">
-import { EnhancedWebSocket } from '@wthe/utils-core'
+import { useWebSocket } from '@wthe/utils-core'
 import { ref } from 'vue'
 
 const message = ref('')
-const ws = new EnhancedWebSocket('ws://127.0.0.1:8080', { immediate: false }, { onMessage(data: any) {
+
+const ws = useWebSocket('ws://127.0.0.1:8080', { immediate: false }, { onMessage(data: any) {
   message.value = data
 } })
-const { connect, send, close } = new EnhancedWebSocket('ws://127.0.0.1:8090', { immediate: false })
+const { connect } = useWebSocket('ws://127.0.0.1:8090', { immediate: false })
 </script>
 
 <template>
-  <button @click="ws.connect">
-    连接:8080
-  </button>
-  <button @click="ws.send('你好，服务器！')">
-    发送:8080
-  </button>
-  <button @click="ws.close">
-    断开:8080
-  </button>
-  <button @click="connect">
-    连接:8090
-  </button>
-  <button @click="send('你好，服务器！')">
-    发送:8090
-  </button>
-  <button @click="close">
-    断开:8090
-  </button>
-  <span>
-    {{ message }}
-  </span>
+  <div>
+    <button @click="ws.connect">
+      连接:8080
+    </button>
+    <button @click="ws.pauseHeartbeat">
+      暂停心跳:8080
+    </button>
+    <button @click="ws.resumeHeartbeat">
+      继续心跳:8080
+    </button>
+    <button @click="ws.send('你好，服务器！')">
+      发送:8080
+    </button>
+    <button @click="ws.close">
+      断开:8080
+    </button>
+  </div>
+  <div>
+    <button @click="connect">
+      连接:8090
+    </button>
+  </div>
+  <div>
+    <span>
+      {{ message }}
+    </span>
+  </div>
 </template>
