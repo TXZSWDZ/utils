587a0db782ee580034dd3cb3bc4bbfbaf83a69c6
WangZe <wangze@zb-software.com>
2025-06-12 17:17:46 +0800

    refactor：重构StorageProxy类

--- packages/core/useStorage/index.ts
+++ packages/core/useStorage/index.ts
@@ -7,138 +7,133 @@ interface StorageEventHooks {
   beforeClear?: () => boolean
 }
 
-interface Config {
+interface GetConfig {
+  needParse?: boolean
+}
+
+interface SetConfig {
   merge?: boolean
   expires?: boolean | number
 }
 
 interface options {
   type?: 'local' | 'session'
-  config?: Config & StorageEventHooks
+  config?: SetConfig & StorageEventHooks
 }
 
 interface StorageService {
   get: (key: string) => any
-  set: (key: string, value: any, options?: Config) => void
+  set: (key: string, value: any, options?: SetConfig) => void
   remove: (key: string) => void
   clear: () => void
 }
 
 const storageInstances = new Map<'local' | 'session', StorageService>()
 
-export function useStorage(
-  options?: options,
-): StorageService {
-  class StorageProxy {
-    private config: Config & StorageEventHooks
-    private storage: Storage
-    constructor(storage: Storage, config: Config) {
-      this.storage = storage
-      this.config = {
-        afterGet: (key, value) => value,
-        beforeSet: (key, value) => [key, value],
-        beforeRemove: key => key,
-        beforeClear: () => true,
-        merge: false,
-        expires: false,
-        ...config,
-      }
+function readValue(value: any, options: GetConfig = {}) {
+  if (options.needParse) {
+    try {
+      return JSON.parse(value)
     }
-
-    private getExpiryTime(options?: Config): number | boolean {
-      const { expires: setExpires = false } = options || {}
-
-      const { expires: configExpires } = this.config
-
-      const expires = setExpires || configExpires
-
-      if (!expires)
-        return false
-
-      if (typeof expires === 'number')
-        return Date.now() + expires
-
-      return Date.now() + 1000 * 60 * 60 * 24 * 30
+    catch (e) {
+      return value
     }
+  }
+  else {
+    return value
+  }
+}
 
-    // TODO 提取处理JSON.parse
-    getItem(key: string): any {
-      const value = this.storage.getItem(key)
+function getExpiryTime(expires: boolean | number): number | boolean {
+  if (typeof expires === 'number')
+    return Date.now() + expires
 
-      if (value === null)
-        return this.config.afterGet!(key, null)
+  return Date.now() + 1000 * 60 * 60 * 24 * 30
+}
 
-      const { value: storedValue, expiry } = JSON.parse(value)
-
-      if (expiry) {
-        if (Date.now() > expiry) {
-          this.removeItem(key)
-          return this.config.afterGet!(key, null)
-        }
-        else {
-          return this.config.afterGet!(key, storedValue)
-        }
-      }
-      else {
-        const storedValue = JSON.parse(value)
-        return this.config.afterGet!(key, storedValue)
-      }
-    }
-
-    // TODO 提取处理JSON.stringify
-    setItem(key: string, value: any, options?: Config): void {
-      const { merge: setMerge = undefined } = options || {}
-      const { merge: configMerge } = this.config
-
-      let [newKey, newValue] = this.config.beforeSet!(key, value)
-
-      let isMerge
-      if (setMerge !== undefined) {
-        isMerge = setMerge
-      }
-      else {
-        isMerge = configMerge
-      }
-
-      if (isMerge) {
-        const storedValue = this.getItem(newKey)
-
-        if (storedValue) {
-          newValue = useMerge(storedValue, newValue)
-        }
-      }
-
-      const expiry = this.getExpiryTime(options)
-
-      if (expiry) {
-        const payload = {
-          value: newValue,
-          expiry,
-        }
-        newValue = JSON.stringify(payload)
-      }
-      else {
-        newValue = JSON.stringify(newValue)
-      }
-
-      this.storage.setItem(newKey, newValue)
-    }
-
-    removeItem(key: string): void {
-      const newKey = this.config.beforeRemove!(key)
-
-      this.storage.removeItem(newKey)
-    }
-
-    clear(): void {
-      const shouldClear = this.config.beforeClear!()
-
-      if (shouldClear) {
-        this.storage.clear()
-      }
+export class StorageProxy {
+  private config: SetConfig & StorageEventHooks
+  private storage: Storage
+  constructor(storage: Storage, config: SetConfig) {
+    this.storage = storage
+    this.config = {
+      afterGet: (key, value) => value,
+      beforeSet: (key, value) => [key, value],
+      beforeRemove: key => key,
+      beforeClear: () => true,
+      merge: false,
+      expires: false,
+      ...config,
     }
   }
 
+  getItem(key: string, options?: GetConfig): any
+  getItem<T>(key: string, options?: GetConfig): T
+  getItem(key: string, options: GetConfig = {}) {
+    const payload = readValue(this.storage.getItem(key), { needParse: true })
+
+    if (payload === null)
+      return this.config.afterGet!(key, null)
+
+    if (payload.expiry) {
+      if (Date.now() > payload.expiry) {
+        this.removeItem(key)
+        return this.config.afterGet!(key, null)
+      }
+      else {
+        return this.config.afterGet!(key, payload.value)
+      }
+    }
+    else {
+      return this.config.afterGet!(key, readValue(payload, options))
+    }
+  }
+
+  setItem(key: string, value: any, options?: SetConfig): void {
+    const finalOptions = { ...this.config, ...options }
+
+    let [newKey, newValue] = this.config.beforeSet!(key, value)
+
+    if (finalOptions.merge) {
+      const storedValue = this.getItem(newKey)
+
+      if (storedValue) {
+        newValue = useMerge(storedValue, newValue)
+      }
+    }
+
+    if (typeof newValue !== 'string') {
+      if (finalOptions.expires) {
+        const payload = {
+          value: newValue,
+          expiry: getExpiryTime(finalOptions.expires),
+        }
+        newValue = payload
+      }
+      newValue = JSON.stringify(newValue)
+    }
+
+    this.storage.setItem(newKey, newValue)
+  }
+
+  removeItem(key: string): void {
+    const newKey = this.config.beforeRemove!(key)
+
+    this.storage.removeItem(newKey)
+  }
+
+  clear(): void {
+    const shouldClear = this.config.beforeClear!()
+
+    if (shouldClear) {
+      this.storage.clear()
+    }
+  }
+}
+
+export function useStorage(
+  options?: options,
+): StorageService {
   const { type = 'local', config = {} } = options || {}
   if (storageInstances.has(type)) {
     return storageInstances.get(type)!
@@ -150,7 +145,7 @@ export function useStorage(
 
   const service = {
     get: (key: string) => storageInstance.getItem(key),
-    set: (key: string, value: any, options?: Config) => { storageInstance.setItem(key, value, options) },
+    set: (key: string, value: any, options?: SetConfig) => { storageInstance.setItem(key, value, options) },
     remove: (key: string) => { storageInstance.removeItem(key) },
     clear: () => { storageInstance.clear() },
   }
