bd7c8e550fb2c41bb6dc3f46df79fd125e83dfe4
WangZe <wangze@zb-software.com>
2025-09-19 09:55:08 +0800

    feat：类型判断工具函数提取isType函数

--- packages/shared/src/is/index.ts
+++ packages/shared/src/is/index.ts
@@ -13,42 +13,82 @@ export function toTypeString(value: unknown): string {
   return objectToString.call(value)
 }
 
-export function isMap(val: unknown): val is Map<any, any> {
-  return toTypeString(val) === IsFlags.MAP
+function isType<T>(val: unknown, type: IsFlags): val is T {
+  return toTypeString(val) === type
 }
 
-export function isSet(val: unknown): val is Set<any> {
-  return toTypeString(val) === IsFlags.SET
+export function isString(val: unknown): val is string {
+  return typeof val === 'string'
 }
 
-export function isDate(val: unknown): val is Date {
-  return toTypeString(val) === IsFlags.DATE
+export function isNumber(val: unknown): val is number {
+  return typeof val === 'number'
 }
 
-export function isRegExp(val: unknown): val is RegExp {
-  return toTypeString(val) === IsFlags.REGEXP
+export function isBoolean(val: unknown): val is boolean {
+  return typeof val === 'boolean'
 }
 
-export function isObject(val: unknown): val is Record<any, any> {
-  return toTypeString(val) === IsFlags.OBJECT
+export function isSymbol(val: unknown): val is symbol {
+  return typeof val === 'symbol'
 }
-
-export const isArray: typeof Array.isArray = Array.isArray
 
 export function isFunction(val: unknown): val is Function {
   return typeof val === 'function'
 }
 
-export function isPromise<T = any>(val: unknown): val is Promise<T> {
-  return (
-    (toTypeString(val) === IsFlags.PROMISE
-      || isFunction(val))
-    && isFunction((val as any).then)
-    && isFunction((val as any).catch)
-    && isFunction((val as any).finally)
-  )
+export function isObject(val: unknown): val is Record<any, any> {
+  return isType<Record<any, any>>(val, IsFlags.OBJECT)
 }
 
-export const isSymbol = (val: unknown): val is symbol => typeof val === 'symbol'
+export const isArray: typeof Array.isArray = Array.isArray
 
-export const isString = (val: unknown): val is string => typeof val === 'string'
+export function isMap(val: unknown): val is Map<any, any> {
+  return isType<Map<any, any>>(val, IsFlags.MAP)
+}
+
+export function isSet(val: unknown): val is Set<any> {
+  return isType<Set<any>>(val, IsFlags.SET)
+}
+
+export function isDate(val: unknown): val is Date {
+  return isType<Date>(val, IsFlags.DATE)
+}
+
+export function isRegExp(val: unknown): val is RegExp {
+  return isType<RegExp>(val, IsFlags.REGEXP)
+}
+
+export function isFormData(val: unknown): val is FormData {
+  return typeof val !== 'undefined' && val instanceof FormData
+}
+
+export function isPromise<T = any>(val: unknown): val is Promise<T> {
+  if (!val)
+    return false
+
+  if (typeof val !== 'object' && typeof val !== 'function')
+    return false
+
+  if (isType<Promise<T>>(val, IsFlags.PROMISE)) {
+    return true
+  }
+
+  const obj = val as { then?: Function, catch?: Function, finally?: Function }
+
+  // Promise
+  if (typeof obj.then === 'function'
+    && typeof obj.catch === 'function'
+    && typeof obj.finally === 'function') {
+    return true
+  }
+
+  // PromiseLike
+  if (typeof obj.then === 'function'
+    && obj.catch === undefined
+    && obj.finally === undefined) {
+    return true
+  }
+
+  return false
+}
