b2621b0930a3fbe0580876c09364b778e78e5dd5
WangZe <wangze@zb-software.com>
2025-09-19 17:45:23 +0800

    test：useDebounce工具函数的实际使用

---
+++ playgrounds/vite/src/components/useDebounce.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { useDebounce } from '@wthe/utils-shared'
+import { ref } from 'vue'
+
+const count = ref<number>(0)
+
+const { execute, cancel } = useDebounce({
+  fn: () => {
+    count.value++
+  },
+})
+
+function handleClick() {
+  execute()
+}
+</script>
+
+<template>
+  <button @click="handleClick">
+    点击
+  </button>
+  <button @click="cancel">
+    取消
+  </button>
+  <div>count : {{ count }}</div>
+</template>
